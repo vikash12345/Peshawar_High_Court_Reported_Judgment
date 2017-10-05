770a374c53addd640c621d28e87cfb340ce29965
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-10-05 11:42:23 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -53,7 +53,7 @@
    $f = $element->find("td", 5)->plaintext;
    $g = $element->find("td", 6)->plaintext;
    $h = $element->find("td/a", 0)->href;
-  	echo "$h\n";
+  	echo "$g\n";
   
   $record = array( 'srno' =>$a, 
 		   'case' => $b,
